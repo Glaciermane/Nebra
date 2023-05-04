ecb4ff2c0ade2dc59752a9fae7ed8c7eca427555
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-04 09:35:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,113 @@
-npm install express discord.js
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const sidePanel = document.querySelector('.sidepanel');
+const collapse = document.querySelector('.collapse');
+const expand = document.querySelector('.expand');
+const rightCol = document.querySelector('.right-col')
 
-const express = require('express');
-const app = express();
+const section = document.getElementsByClassName('section');
+const subSection = document.getElementsByClassName('sub-section');
+const title = document.getElementsByClassName('title');
 
-app.get('/api/players', (req, res) => {
-  const players = [
-    { name: 'Player 1', tier: 10, achievements: 1000 },
-    { name: 'Player 2', tier: 5, achievements: 500 },
-    { name: 'Player 3', tier: 20, achievements: 2000 },
-  ];
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
-  res.json(players);
-});
+console.log("Loaded.")
 
-app.listen(3000, () => {
-  console.log('API server listening on port 3000');
-});
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
 
-const fetch = require('node-fetch');
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-500%';
+    mainMenu.style.display = 'flex';
+}
 
-fetch('http://localhost:3000/api/players')
-  .then(res => res.json())
-  .then(players => {
-    console.log(players);
-  });
+function openSidePanel() {
+    sidePanel.style.display = "block";
+    collapse.style.display = 'block';
+    expand.style.display = "none"
+}
+
+function closeSidePanel() {
+    sidePanel.style.display = "none";
+    collapse.style.display = 'none';
+    expand.style.display = "block"
+}
+
+if (width < 900) {
+    title[0].onclick = function () {
+        closeSidePanel()
+    }
+    title[1].onclick = function () {
+        closeSidePanel()
+    }
+    title[2].onclick = function () {
+        closeSidePanel()
+    }
+    title[3].onclick = function () {
+        closeSidePanel()
+    }
+    title[4].onclick = function () {
+        closeSidePanel()
+    }    
+}
+
+if (width < 900) {
+    section[0].onclick = function () {
+        closeSidePanel()
+    }
+    section[1].onclick = function () {
+        closeSidePanel()
+    }
+    section[2].onclick = function () {
+        closeSidePanel()
+    }
+    section[3].onclick = function () {
+        closeSidePanel()
+    }
+    section[4].onclick = function () {
+        closeSidePanel()
+    }    
+    section[5].onclick = function () {
+        closeSidePanel()
+    }    
+    section[6].onclick = function () {
+        closeSidePanel()
+    }    
+    section[7].onclick = function () {
+        closeSidePanel()
+    }    
+    section[8].onclick = function () {
+        closeSidePanel()
+    }    
+    section[9].onclick = function () {
+        closeSidePanel()
+    }    
+    section[10].onclick = function () {
+        closeSidePanel()
+    }    
+}
+
+if (width < 650) {
+    subSection[0].onclick = function () {
+        closeSidePanel()
+    }
+    subSection[1].onclick = function () {
+        closeSidePanel()
+    }
+}
+
+var myVar;
+
+function loaderFunction() {
+  myVar = setTimeout(showPage, 1400);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("page").style.display = "block";
+}
