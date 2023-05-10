0bf182b0e05dc1d61343f2857d1419b57982603f
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-10 13:54:16 +0200

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -1,15 +1,16 @@
 function searchRankings() {
-  // Wert aus der Suchleiste auslesen
-  var searchValue = document.getElementById('search-input').value.toLowerCase();
-
-  // Alle Ranking-Reihen durchlaufen und überprüfen, ob der Spielername enthalten ist
-  var rows = document.getElementsByClassName('ranking-row');
+  // Suchbegriff aus dem Eingabefeld abrufen
+  var searchInput = document.getElementById("search-input").value.toLowerCase();
+  
+  // Alle Zeilen in der Rangliste ausblenden
+  var rows = document.getElementsByClassName("ranking-row");
   for (var i = 0; i < rows.length; i++) {
-    var playerName = rows[i].getElementsByClassName('ranking-column')[0].innerText.toLowerCase();
-    if (playerName.includes(searchValue)) {
-      rows[i].style.display = ''; // Zeile anzeigen
-    } else {
-      rows[i].style.display = 'none'; // Zeile ausblenden
-    }
+    rows[i].style.display = "none";
+  }
+  
+  // Zeile mit dem gesuchten Spieler anzeigen
+  var playerRow = document.getElementById(searchInput);
+  if (playerRow) {
+    playerRow.style.display = "flex";
   }
 }
