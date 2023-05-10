8e4412495a62be136cbfff92608d726e70515994
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-10 15:17:37 +0200

    Create discordbot.js

---
+++ pages/discordbot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+const token = 'MTEwMzU4Njk2NDkzMTg3MDczMA.GByJRM.ElIVEbvCx2Niz7wV2c8bs28nH5DSczqJvfW0z0';
+const channelId = '1101111620215779368';
+
+bot.login(token);
+
+bot.on('ready', () => {
+  console.log('Bot is ready');
+});
+
+function sendDiscordMessage(message) {
+  const channel = bot.channels.cache.get(channelId);
+  if (channel) {
+    channel.send(message);
+  } else {
+    console.log('Channel not found');
+  }
+}
+
+// Diese Funktion kann aufgerufen werden, um die Benachrichtigung zu senden
+function sendNotificationToDiscord(name, discordName, tier, message) {
+  const notificationMessage = `New rank request!\n\nIngame Name: ${name}\nDiscord Name: ${discordName}\nTier: ${tier}\nNachricht: ${message}`;
+  sendDiscordMessage(notificationMessage);
+}
+
+module.exports = {
+  sendNotificationToDiscord
+};
