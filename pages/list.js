9e792b9442c40d511719b417032996bc1000a4eb
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-15 06:57:41 +0200

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -4,14 +4,16 @@ function searchRankings() {
 
   for (var i = 0; i < rows.length; i++) {
     var playerNameElement = rows[i].getElementsByClassName("column-cell-name")[0];
-    
-    if (playerNameElement) {
-      var playerName = playerNameElement.innerText.toLowerCase();
+    var top3Element = rows[i].getElementsByClassName("column-cell-top3")[0];
 
-      if (playerName.includes(input)) {
-        rows[i].style.display = ""; // Zeige die Zeile, wenn der Spielername übereinstimmt
+    if (playerNameElement && top3Element) {
+      var playerName = playerNameElement.innerText.toLowerCase();
+      var top3Text = top3Element.innerText.toLowerCase();
+
+      if (playerName.includes(input) || top3Text.includes(input)) {
+        rows[i].style.display = ""; // Zeige die Zeile, wenn der Spielername oder Top3 übereinstimmt
       } else {
-        rows[i].style.display = "none"; // Verstecke die Zeile, wenn der Spielername nicht übereinstimmt
+        rows[i].style.display = "none"; // Verstecke die Zeile, wenn der Spielername oder Top3 nicht übereinstimmt
       }
     }
   }
