4006f0371bc76c4b21e0951c1f0c9e7d4a6ffdf0
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-15 09:24:04 +0200

    Update sort.js

--- pages/sort.js
+++ pages/sort.js
@@ -1,42 +1,41 @@
+function sortTable(columnIndex) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("rankingTable");
+  switching = true;
 
-    function sortTable(columnIndex) {
-      var table, rows, switching, i, x, y, shouldSwitch;
-      table = document.getElementById("rankingTable");
-      switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
 
-      while (switching) {
-        switching = false;
-        rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[columnIndex];
+      y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
 
-        for (i = 1; i < (rows.length - 1); i++) {
-          shouldSwitch = false;
-          x = rows[i].getElementsByTagName("TD")[columnIndex];
-          y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+      if (columnIndex === 2) { // Index 2 für Spalte "column-cell-tier"
+        var tierOrder = ['s+', 's', 'a', 'b', 'c', 'd', 'e'];
+        var xIndex = tierOrder.indexOf(x.innerHTML.toLowerCase());
+        var yIndex = tierOrder.indexOf(y.innerHTML.toLowerCase());
 
-          if (columnIndex === 2) { // Index 2 für Spalte "column-cell-tier"
-            var tierOrder = ['s+', 's', 'a', 'b', 'c', 'd', 'e'];
-            var xIndex = tierOrder.indexOf(x.innerHTML.toLowerCase());
-            var yIndex = tierOrder.indexOf(y.innerHTML.toLowerCase());
-
-            if (xIndex > yIndex) {
-              shouldSwitch = true;
-              break;
-            }
-          } else {
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-              shouldSwitch = true;
-              break;
-            }
-          }
+        if (xIndex > yIndex) {
+          shouldSwitch = true;
+          break;
         }
-
-        if (shouldSwitch) {
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-          switching = true;
+      } else {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
         }
       }
     }
 
-    window.addEventListener('load', function() {
-      sortTable(2); // Sortiere nach Spalte "column-cell-tier" beim Laden der Seite
-    });
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+window.addEventListener('load', function() {
+  sortTable(2); // Sortiere nach Spalte "column-cell-tier" beim Laden der Seite
+});
