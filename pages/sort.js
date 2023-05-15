187d0c878a0423efa83b72fa3a9748ee5b8157b6
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-15 11:16:45 +0200

    Update sort.js

--- pages/sort.js
+++ pages/sort.js
@@ -22,7 +22,7 @@ function sortTable(columnIndex) {
           break;
         }
       } else {
-        if (x.innerHTML.toLowerCase().localeCompare(y.innerHTML.toLowerCase()) > 0) {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
           shouldSwitch = true;
           break;
         }
