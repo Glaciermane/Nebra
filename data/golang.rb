e42f5355491654b459f10722f5238165cedbd0a3
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-15 13:17:08 +0200

    Update golang.rb

--- data/golang.rb
+++ data/golang.rb
@@ -1,1 +1,9 @@
-
+sudo yum install -y git mercurial
+cd /usr/local
+sudo wget <Go-Download-Link>
+  sudo tar -C /usr/local -xzf go*.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+source /etc/profile
+go version
+/// ungoat ///
