cb6259242529aed89de44984ac408484fdc73ac3
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-15 07:54:29 +0200

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -1,17 +1,20 @@
 function searchRankings() {
+  // Eingabewert aus dem Suchfeld abrufen
   var input = document.getElementById("searchInput").value.toLowerCase();
+
+  // Alle Zeilen der Tabelle abrufen
   var rows = document.getElementsByClassName("ranking-row");
 
+  // Schleife über alle Zeilen
   for (var i = 0; i < rows.length; i++) {
     var nameCell = rows[i].getElementsByClassName("column-cell-name")[0];
     var top3Cell = rows[i].getElementsByClassName("column-cell-top3")[0];
 
-    if (nameCell && top3Cell) { // Überprüfen, ob die Zellen gefunden wurden
-      if (nameCell.textContent.toLowerCase().indexOf(input) > -1 || top3Cell.textContent.toLowerCase().indexOf(input) > -1) {
-        rows[i].style.display = ""; // Zeile anzeigen
-      } else {
-        rows[i].style.display = "none"; // Zeile ausblenden
-      }
+    // Prüfen, ob der Name oder der Top 3-Wert dem Suchbegriff entsprechen
+    if (nameCell.textContent.toLowerCase().indexOf(input) > -1 || top3Cell.textContent.toLowerCase().indexOf(input) > -1) {
+      rows[i].style.display = ""; // Zeile anzeigen
+    } else {
+      rows[i].style.display = "none"; // Zeile ausblenden
     }
   }
 }
