f06faa2c89a49ffd0b310aaa077a91cbe3f2563b
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-16 08:37:02 +0200

    Update superuser.js

--- pages/superuser.js
+++ pages/superuser.js
@@ -1,14 +1,16 @@
 function login() {
   var superuser = document.getElementById("superuser").value;
   var pass = document.getElementById("pass").value;
+  var errorElement = document.getElementById("error-message");
 
   // Überprüfen Sie hier die Superuser-Daten und führen Sie entsprechende Aktionen durch
 
   if (superuser === "admin" && pass === "12345") {
     // Superuser-Daten sind korrekt, führen Sie hier den gewünschten Code aus
+    errorElement.textContent = ""; // Leeren Sie die Fehlermeldung
     console.log("Superuser eingeloggt!");
   } else {
     // Superuser-Daten sind nicht korrekt, zeigen Sie eine Fehlermeldung an
-    console.log("Falsche Superuser-Daten!");
+    errorElement.textContent = "Falsche Superuser-Daten!";
   }
 }
