2105a8a974e08c8a8c54f2da46e9a04fa35b1748
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-16 08:09:40 +0200

    Update superuser.js

--- data/superuser.js
+++ data/superuser.js
@@ -1,16 +1,9 @@
-import superpass from './config.js';
-
-console.log('pass:', superpass);
-// Weitere Aktionen mit dem Passwort durchführen
-
-document.getElementById('login-form').addEventListener('submit', function(event) {
+function login(event) {
   event.preventDefault(); // Verhindert das Standard-Formularverhalten
 
   const username = document.getElementById('superuser').value;
   const password = document.getElementById('pass').value;
-
-  // Hier können Sie den Benutzernamen und das Passwort überprüfen
-  // Verwenden Sie beispielsweise AJAX, um eine Anfrage an den Server zu senden
+  const superpass = process.env.SUPER_PASS;
 
   if (username === 'superuser' && password === superpass) {
     // Authentifizierung erfolgreich, weiterleiten zur geschützten Seite
@@ -18,4 +11,4 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   } else {
     alert('Ungültige Anmeldeinformationen');
   }
-});
+}
