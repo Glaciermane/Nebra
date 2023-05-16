67cb853e8a010054392dadd85b6a34d85adae16e
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-16 08:42:40 +0200

    Update superuser.js

--- pages/superuser.js
+++ pages/superuser.js
@@ -1,4 +1,6 @@
-function login() {
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Verhindert das automatische Neuladen der Seite nach dem Absenden des Formulars
+
   var superuser = document.getElementById("superuser").value;
   var pass = document.getElementById("pass").value;
   var errorElement = document.getElementById("error-message");
@@ -13,4 +15,4 @@ function login() {
     // Superuser-Daten sind nicht korrekt, zeigen Sie eine Fehlermeldung an
     errorElement.textContent = "Falsche Superuser-Daten!";
   }
-}
+});
