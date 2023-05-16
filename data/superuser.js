9a1090777f4de4a3203cf6110de1c8e35c1a236a
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-16 08:20:01 +0200

    Update superuser.js

--- data/superuser.js
+++ data/superuser.js
@@ -5,7 +5,7 @@ function login(event) {
   const password = document.getElementById('pass').value;
   const superpass = process.env.SUPER_PASS;
 
-  if (username === 'superuser' && password === superpass) {
+  if (username === 'superuser' && password === 'pass') {
     // Authentifizierung erfolgreich, weiterleiten zur geschützten Seite
     window.location.href = 'supervisor.html';
   } else {
