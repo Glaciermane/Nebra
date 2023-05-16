58474fc556ba6c62f1eabecdbf0581ca30a37d6f
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-05-16 07:44:16 +0200

    Update superuser.js

--- data/superuser.js
+++ data/superuser.js
@@ -1,3 +1,10 @@
+import superpass from './config.js';
+
+console.log('pass:', superpass);
+// Weitere Aktionen mit dem Passwort durchführen
+
+
+
 document.getElementById('login-form').addEventListener('submit', function(event) {
   event.preventDefault(); // Verhindert das Standard-Formularverhalten
 
@@ -7,7 +14,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   // Hier können Sie den Benutzernamen und das Passwort überprüfen
   // Verwenden Sie beispielsweise AJAX, um eine Anfrage an den Server zu senden
 
-  if (username === 'superuser' && password === 'passcode') {
+  if (username === 'superuser' && password === superpass) {
     // Authentifizierung erfolgreich, weiterleiten zur geschützten Seite
     window.location.href = 'supervisor.html';
   } else {
